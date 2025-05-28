a53dc8031850d6985e9665ac28ae373f196dbd51
pateljayconestoga <jpatel2044@conestogac.on.ca>
2025-05-28 19:17:37 -0400

    Add networking to terraform main file.

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  access_key = "ACCESS_KEY"
-  secret_key = "SECRET_KEY"
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 # DATA
@@ -18,11 +16,67 @@ resource "aws_vpc" "app" {
 
 }
 
+
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.app.id
+  tags = {
+    Name = "IGW-CondoMatics-S25"
+  }
+}
+
+
+# SUBNET
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.app.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+
+# ROUTE TABLE
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.app.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+
+# ROUTE TABLE ASSOCIATION
+resource "aws_route_table_association" "pulic_subnet_route_table" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+# SECURITY GROUP
+resource "aws_security_group" "public_security_group" {
+  name   = "public_security_group"
+  vpc_id = aws_vpc.app.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # INSTANCES
 resource "aws_instance" "nginx1" {
-  ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type          = "t3.micro"
-  
+  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.public_subnet.id
+
   user_data = <<EOF
 #! /bin/bash
 sudo amazon-linux-extras install -y nginx1
