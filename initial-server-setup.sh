d10dd0e5860e64fe2cff63badfbb8fa3dc149210
pateljayconestoga <jpatel2044@conestogac.on.ca>
2025-06-11 19:06:13 -0400

    Created instance profile and copy files in s3

--- initial-server-setup.sh
+++ initial-server-setup.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 sudo amazon-linux-extras install -y nginx1
 sudo service nginx start
+
+#aws s3 cp -r s3://S{aws_s3_bucket.web_bucket.id//webcontent/ /home/ec2-user/ 
+#aws s3 sync s3://$(aws_s3_bucket.web_bucket.id//webcontent/ /home/ec2-user/
+
+
+aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/webcontent/index.html /home/ec2-user/index.html
+aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/webcontent/style.css /home/ec2-user/styles.css 
+aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/webcontent/campus.jpg /home/ec2-user/campus.jpg 
+aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/webcontent/students.jpg /home/ec2-user/students.jpg 
+aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/webcontent/programs.jpg /home/ec2-user/programs.jpg
+
 sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+
+sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
+sudo cp /home/ec2-user/styles.css /usr/share/nginx/html/styles.css 
+sudo cp /home/ec2-user/campus.jpg /usr/share/nginx/html/campus.jpg 
+sudo cp /home/ec2-user/students.jpg /usr/share/nginx/html/students.jpg 
+sudo cp /home/ec2-user/programs.jpg /usr/share/nginx/html/programs.jpg
--- instances.tf
+++ instances.tf
@@ -1,7 +1,10 @@
 # INSTANCES
 resource "aws_instance" "nginx1" {
-  ami             = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type   = "t3.micro"
+  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type = "t3.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
+
   subnet_id       = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.public_security_group.id]
 
@@ -9,10 +12,18 @@ resource "aws_instance" "nginx1" {
 }
 
 resource "aws_instance" "nginx2" {
-  ami             = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type   = "t3.micro"
+  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type = "t3.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
+
   subnet_id       = aws_subnet.public_subnet_2.id
   security_groups = [aws_security_group.public_security_group.id]
 
   user_data = file("./initial-server-setup.sh")
+}
+
+resource "aws_iam_instance_profile" "nginx_profile" {
+  name = "nginx-profile"
+  role = "LabRole"
 }
